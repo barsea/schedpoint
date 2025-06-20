5ddf478b559550853bef46d82dbca535a28a2d40
barsea <92622141+barsea@users.noreply.github.com>
2025-06-20 22:27:27 +0900

    日付のダミーデータを作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <!-- ここに、これからスケッチを元にカレンダーのHTMLを記述していく -->
   <div class="p-4">
+    <h1 class="text-2xl font-bold text-blue-600">2025年6月20日</h1>
     <h1 class="text-2xl font-bold text-blue-600">SchedPoint</h1>
     <p>ここにカレンダーのヘッダーや本体を作っていきます。</p>
   </div>

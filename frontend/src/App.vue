883eff739479130c81fcfda5da2c964cdb23008a
barsea <92622141+barsea@users.noreply.github.com>
2025-06-22 18:41:01 +0900

    ScheduleView.vueをルートコンポーネントに設定

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,9 @@
 </script>
 
 <template>
-  <Header />
-  <div class="p-4">
-    <h1 class="text-2xl font-bold text-blue-600">2025年6月20日</h1>
-    <h1 class="text-2xl font-bold text-blue-600">SchedPoint</h1>
-    <p>ここにカレンダーのヘッダーや本体を作っていきます。</p>
-  </div>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style scoped>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ScheduleView from '../views/ScheduleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'schedule',
+      component: ScheduleView,
     },
     {
       path: '/about',

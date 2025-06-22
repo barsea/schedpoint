eb87f9dd4e2b6b62802ab2ab7a856dfb50b4e0c7
barsea <92622141+barsea@users.noreply.github.com>
2025-06-22 15:34:26 +0900

    Header.vueコンポーネントを作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+  import Header from './components/Header.vue'
 </script>
 
 <template>
+  <Header />
   <div class="p-4">
     <h1 class="text-2xl font-bold text-blue-600">2025年6月20日</h1>
     <h1 class="text-2xl font-bold text-blue-600">SchedPoint</h1>

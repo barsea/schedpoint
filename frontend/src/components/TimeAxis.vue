628a6c2091cea4a133baf796fc297ea39a47d6a3
barsea <92622141+barsea@users.noreply.github.com>
2025-06-28 09:57:37 +0900

    TimeAxisに24個の枠を追加

--- frontend/src/components/TimeAxis.vue
+++ frontend/src/components/TimeAxis.vue
@@ -1,3 +1,11 @@
 <template>
-  時間軸
+  <div>
+    <div v-for="n in 24" :key="n" class="relative h-12 pr-2 text-right border-b border-gray-200"></div>
+  </div>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>

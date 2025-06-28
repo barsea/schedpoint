114db3e120b157d1e10c83c650639564ac1c124b
barsea <92622141+barsea@users.noreply.github.com>
2025-06-28 11:04:51 +0900

    TimeAxisに各時刻を記述

--- frontend/src/components/TimeAxis.vue
+++ frontend/src/components/TimeAxis.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <div v-for="n in 24" :key="n" class="relative h-12 pr-2 text-right border-b border-gray-200"></div>
+    <div
+      v-for="(text, index) in items"
+      :key="index"
+      class="relative h-12 pr-2 text-right border-b border-gray-200"
+    >
+      {{ text }}
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  '', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
+  '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
+  '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'
+]);
 </script>
 
 <style scoped>

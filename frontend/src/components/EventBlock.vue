cc2b4ce753241d6c16744a58bbdc3ce343fb1c86
barsea <92622141+barsea@users.noreply.github.com>
2025-07-05 12:25:20 +0900

    カテゴリ名と時間をブロックの垂直方向中央に表示

--- frontend/src/components/EventBlock.vue
+++ frontend/src/components/EventBlock.vue
@@ -12,10 +12,8 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="absolute inset-x-0 bg-blue-200 opacity-75 z-10 p-2 rounded flex flex-col justify-between"
-  >
-    <div class="flex items-center justify-between">
+  <div class="absolute inset-x-0 bg-blue-200 opacity-75 z-10 p-2 rounded flex items-center">
+    <div class="flex items-center justify-between w-full">
       <p class="font-bold text-sm truncate">{{ categoryName }}</p>
       <p class="text-xs flex-shrink-0 ml-2">{{ time }}</p>
     </div>

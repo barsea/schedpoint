9dc1b76b9d6ded071a68adf7aa0b9f058b7c1266
barsea <92622141+barsea@users.noreply.github.com>
2025-07-06 14:54:32 +0900

    FontAwesomeを導入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// --- Font Awesome の設定をここから ---
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faBed, // 睡眠
+  faUtensils, // 食事
+  faMobileAlt, // スマホ
+  faLaptop, // PC
+  faGamepad, // ゲーム
+  faBookOpen, // 勉強
+  faBath, // お風呂
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBed, faUtensils, faMobileAlt, faLaptop, faGamepad, faBookOpen, faBath)
+// --- Font Awesome の設定はここまで ---
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')

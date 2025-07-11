a11fe2ffafc6e396766c7f0709b913d3de470d87
barsea <92622141+barsea@users.noreply.github.com>
2025-07-11 17:42:37 +0900

    testディレクトリ削除、RSpecを導入

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,11 @@ module Schedpoint
       g.stylesheets false
       g.javascripts false
       g.helper false
+      g.test_framework :rspec, 
+                        fixtures: false,
+                        view_specs: false, 
+                        helper_specs: false, 
+                        routing_specs: false
     end
 
     # Configuration for the application, engines, and railties goes here.

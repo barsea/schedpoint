6e5c9765a8742988c7baf5fbfce09dcbb08895c9
barsea <92622141+barsea@users.noreply.github.com>
2025-07-14 16:58:15 +0900

    style: RuboCopを導入しコードスタイルを自動整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,8 +9,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  has_many :plans
-  has_many :actuals
+  has_many :plans, dependent: :destroy
+  has_many :actuals, dependent: :destroy
 
   validates :name, presence: true
 end

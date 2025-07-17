30f27e2c6b22ed470164a14d3e09b25e6f9549f8
barsea <92622141+barsea@users.noreply.github.com>
2025-07-17 10:36:44 +0900

    feat(frontend): ログイン画面を作成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ScheduleView from '../views/ScheduleView.vue'
 import EventFormView from '@/views/EventFormView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/events/new',
       name: 'event-new',
       component: EventFormView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })

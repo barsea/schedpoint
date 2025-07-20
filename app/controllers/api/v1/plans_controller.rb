a4c27f3532247688c2477ba5ea96ca2ea605ec74
barsea <92622141+barsea@users.noreply.github.com>
2025-07-21 08:56:56 +0900

    feat(api): Plan更新API(update)を実装

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::PlansController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_plan, only: %i[show update]
+
   def index
     date = params[:date] ? Date.parse(params[:date]) : Date.current
     plans = current_user.plans.where(start_time: date.all_day)
@@ -7,13 +9,11 @@ class Api::V1::PlansController < ApplicationController
   end
 
   def show
-    @plan = current_user.plans.find(params[:id])
     render json: @plan
   end
 
   def create
     plan = current_user.plans.build(plan_params)
-
     if plan.save
       render json: plan, status: :created
     else
@@ -21,7 +21,19 @@ class Api::V1::PlansController < ApplicationController
     end
   end
 
+  def update
+    if @plan.update(plan_params)
+      render json: @plan, status: :ok
+    else
+      render json: { errors: @plan.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_plan
+    @plan = current_user.plans.find(params[:id])
+  end
 
   def plan_params
     params.require(:plan).permit(:memo, :start_time, :end_time, :category_id)

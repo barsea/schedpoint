6ff3eeeab948cb437532984fdc3e74ff91863575
barsea <92622141+barsea@users.noreply.github.com>
2025-07-20 17:19:54 +0900

    feat(api): Plan一覧取得APIに日付絞り込み機能を追加
    指定した日付の予定だけを取得するように設定

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::PlansController < ApplicationController
   before_action :authenticate_user!
   def index
-    plans = current_user.plans
+    date = params[:date] ? Date.parse(params[:date]) : Date.current
+    plans = current_user.plans.where(start_time: date.all_day)
     render json: plans
   end
 end

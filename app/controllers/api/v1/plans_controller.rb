f244097b7c333bf73b88e71c0f5c41cce26b42ac
barsea <92622141+barsea@users.noreply.github.com>
2025-07-21 08:33:54 +0900

    feat(api): Plan詳細表示API(show)を実装

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -6,6 +6,11 @@ class Api::V1::PlansController < ApplicationController
     render json: plans
   end
 
+  def show
+    @plan = current_user.plans.find(params[:id])
+    render json: @plan
+  end
+
   def create
     plan = current_user.plans.build(plan_params)
 

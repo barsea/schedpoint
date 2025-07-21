dfef7024cc7d4780eaf66717564a8edb0e3733ad
barsea <92622141+barsea@users.noreply.github.com>
2025-07-21 12:30:58 +0900

    feat(frontend): fetchPlansアクションの実装

--- frontend/src/stores/plan.js
+++ frontend/src/stores/plan.js
@@ -8,5 +8,18 @@ export const usePlanStore = defineStore('plan', {
 
   getters: {},
 
-  actions: {},
+  actions: {
+    /**
+     * @param {string} date
+     */
+    async fetchPlans(date) {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/v1/plans?date=${date}`)
+        this.plans = response.data
+      } catch (error) {
+        console.error('予定の取得に失敗しました:', error)
+        this.plans = []
+      }
+    },
+  },
 })

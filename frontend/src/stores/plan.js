430dc88c63a2806500f595650fe6356eae1903c6
barsea <92622141+barsea@users.noreply.github.com>
2025-07-21 12:24:42 +0900

    feat(frontend): planストアの基本設計

---
+++ frontend/src/stores/plan.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const usePlanStore = defineStore('plan', {
+  state: () => ({
+    plans: [],
+  }),
+
+  getters: {},
+
+  actions: {},
+})
